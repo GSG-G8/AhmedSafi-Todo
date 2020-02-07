210d84050a8cae1505f031b72cb38d2ab1c546ab
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-02-07 19:02:15 +0200

    created addTodo function relates #5

--- js/test.js
+++ js/test.js
@@ -1,7 +1,71 @@
-var test = require('tape');
-var logic = require('./logic');
+const logic = require("./logic");
 
-test('Example test', function(t) {
-  t.pass();
-  t.end();
+describe("should return an array of objects", () => {
+  test("should return an array of objects", () => {
+    const actual = logic.addTodo([], "go home");
+    const expected = [{ todo: "go home", id: 1, done: false }];
+    expect(actual).toEqual(expected);
+  });
+  test("should return an array of objects", () => {
+    const actual = logic.addTodo(
+      [{ todo: "go home", id: 1, done: false }],
+      "play VScode"
+    );
+    const expected = [
+      { todo: "go home", id: 1, done: false },
+      { todo: "play VScode", id: 2, done: false }
+    ];
+    expect(actual).toEqual(expected);
+  });
+  test("should return an array of objects", () => {
+    const actual = logic.addTodo(
+      [
+        { todo: "go home", id: 1, done: false },
+        { todo: "play VScode", id: 2, done: false }
+      ],
+      "go to sleep"
+    );
+    const expected = [
+      { todo: "go home", id: 1, done: false },
+      { todo: "play VScode", id: 2, done: false },
+      { todo: "go to sleep", id: 3, done: false }
+    ];
+    expect(actual).toEqual(expected);
+  });
+  test("should return an array of objects", () => {
+    const actual = logic.addTodo(
+      [
+        { todo: "go home", id: 1, done: false },
+        { todo: "play VScode", id: 2, done: false },
+        { todo: "go to sleep", id: 3, done: false }
+      ],
+      "wake up"
+    );
+    const expected = [
+      { todo: "go home", id: 1, done: false },
+      { todo: "play VScode", id: 2, done: false },
+      { todo: "go to sleep", id: 3, done: false },
+      { todo: "wake up", id: 4, done: false }
+    ];
+    expect(actual).toEqual(expected);
+  });
+  test("should return an array of objects", () => {
+    const actual = logic.addTodo(
+      [
+        { todo: "go home", id: 1, done: false },
+        { todo: "play VScode", id: 2, done: false },
+        { todo: "go to sleep", id: 3, done: false },
+        { todo: "wake up", id: 4, done: false }
+      ],
+      "drink tea"
+    );
+    const expected = [
+      { todo: "go home", id: 1, done: false },
+      { todo: "play VScode", id: 2, done: false },
+      { todo: "go to sleep", id: 3, done: false },
+      { todo: "wake up", id: 4, done: false },
+      { todo: "drink tea", id: 5, done: false }
+    ];
+    expect(actual).toEqual(expected);
+  });
 });
