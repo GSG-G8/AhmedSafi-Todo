16e27c93be2d7af3e858fd4134bfc3640b29ec0a
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-02-07 19:23:08 +0200

    created deleteTodo function relates #7

--- js/test.js
+++ js/test.js
@@ -69,3 +69,61 @@ describe("should return an array of objects", () => {
     expect(actual).toEqual(expected);
   });
 });
+
+describe("should return an array without the deleted object", () => {
+  test("should return an array without the deleted object", () => {
+    const actual = logic.deleteTodo(
+      [
+        { todo: "go home", id: 1, done: true },
+        { todo: "play VScode", id: 2, done: false },
+        { todo: "go to sleep", id: 3, done: false },
+        { todo: "wake up", id: 4, done: false },
+        { todo: "drink tea", id: 5, done: false }
+      ],
+      1
+    );
+    const expected = [
+      { todo: "play VScode", id: 2, done: false },
+      { todo: "go to sleep", id: 3, done: false },
+      { todo: "wake up", id: 4, done: false },
+      { todo: "drink tea", id: 5, done: false }
+    ];
+    expect(actual).toEqual(expected);
+  });
+  test("should return an array without the deleted object", () => {
+    const actual = logic.deleteTodo(
+      [
+        { todo: "play VScode", id: 1, done: false },
+        { todo: "go to sleep", id: 2, done: false },
+        { todo: "wake up", id: 3, done: false },
+        { todo: "drink tea", id: 4, done: false }
+      ],
+      4
+    );
+    const expected = [
+      { todo: "play VScode", id: 1, done: false },
+      { todo: "go to sleep", id: 2, done: false },
+      { todo: "wake up", id: 3, done: false }
+    ];
+    expect(actual).toEqual(expected);
+  });
+  test("should return an array without the deleted object", () => {
+    const actual1 = logic.deleteTodo(
+      [
+        { todo: "play VScode", id: 1, done: false },
+        { todo: "go to sleep", id: 2, done: false },
+        { todo: "wake up", id: 3, done: false }
+      ],
+      2
+    );
+    const actual2 = logic.addTodo(actual1, "drink coffee");
+    const actual3 = logic.addTodo(actual2, "watch anime");
+    const expected = [
+      { todo: "play VScode", id: 1, done: false },
+      { todo: "wake up", id: 3, done: false },
+      { todo: "drink coffee", id: 9, done: false },
+      { todo: "watch anime", id: 10, done: false }
+    ];
+    expect(actual3).toEqual(expected);
+  });
+});
